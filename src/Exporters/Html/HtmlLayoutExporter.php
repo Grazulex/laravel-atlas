583a9d4899cc35a21890fd75d4cdbb58688d34b0
Jean-Marc Strauven <jms@grazulex.be>
2025-07-23 02:58:31 +0200

    Refactor HtmlLayoutExporter to populate commands and services data in render method

--- src/Exporters/Html/HtmlLayoutExporter.php
+++ src/Exporters/Html/HtmlLayoutExporter.php
@@ -15,15 +15,25 @@ class HtmlLayoutExporter
     {
         // Extraire les données selon le type
         $modelsData = [];
+        $commandsData = [];
+        $servicesData = [];
 
         if (isset($data['models']) && is_array($data['models']) && isset($data['models']['data'])) {
             $modelsData = $data['models']['data'];
         }
 
+        if (isset($data['commands']) && is_array($data['commands']) && isset($data['commands']['data'])) {
+            $commandsData = $data['commands']['data'];
+        }
+
+        if (isset($data['services']) && is_array($data['services']) && isset($data['services']['data'])) {
+            $servicesData = $data['services']['data'];
+        }
+
         return View::make('atlas::exports.layout', [
             'models' => $modelsData,
-            'commands' => [], // à remplir plus tard
-            'services' => [], // à remplir plus tard
+            'commands' => $commandsData,
+            'services' => $servicesData,
         ])->render();
     }
 }
