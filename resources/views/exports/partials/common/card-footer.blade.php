5a37fdc64292d7d54053d92ad611de6efb273be6
Jean-Marc Strauven <jms@grazulex.be>
2025-07-24 07:29:24 +0200

    feat: Add card footer component to display class and file information

---
+++ resources/views/exports/partials/common/card-footer.blade.php
@@ -0,0 +1,14 @@
+{{-- Card Footer --}}
+<div class="mt-4 pt-3 border-t border-gray-100 dark:border-gray-600">
+    <div class="flex items-center justify-between text-xs text-gray-500 dark:text-gray-400">
+        <div class="flex items-center space-x-2">
+            <span class="inline-flex items-center">
+                📄 <span class="ml-1 font-mono">{{ $class }}</span>
+            </span>
+        </div>
+        <div class="flex items-center space-x-1 text-gray-400 dark:text-gray-500">
+            <span>📁</span>
+            <span class="font-mono text-[10px]">{{ $file }}</span>
+        </div>
+    </div>
+</div>
